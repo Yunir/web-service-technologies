64c865225a6c918690c59fccd162dd1551a09f51
Yunir Salimzyanov <yunir.me@gmail.com>
2021-03-18 22:57:48 +0300

    Copy J2EE web service from methodical book

---
+++ src/me/yunir/PersonWebService.java
@@ -0,0 +1,45 @@
+package me.yunir;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.Resource;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+import javax.sql.DataSource;
+
+/**
+ * Веб-сервис.
+ * Содержит операции веб-сервиса.
+ * Содержит инъекцию источника данных, настроенного на стороне сервера приложений.
+ */
+@WebService(serviceName = "PersonService")
+public class PersonWebService {
+    @Resource(lookup = "jdbc/ifmo-ws")
+    private DataSource dataSource;
+
+    @WebMethod(operationName = "getAllPersons")
+    public List<Person> getAllPersons() {
+        PostgreSQLDAO dao = new PostgreSQLDAO(getConnection());
+        return dao.getAllPersons();
+    }
+
+    @WebMethod(operationName = "getPersonsByName")
+    public List<Person> getPersonsByName(@WebParam(name = "personName") String name) {
+        PostgreSQLDAO dao = new PostgreSQLDAO(getConnection());
+        return dao.getPersonsByName(name);
+    }
+
+    private Connection getConnection() {
+        Connection result = null;
+        try {
+            result = dataSource.getConnection();
+        } catch (SQLException ex) {
+            Logger.getLogger(PersonWebService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return result;
+    }
+}
