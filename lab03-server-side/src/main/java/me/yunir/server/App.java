d57648e025edc494ab703ca333c6d0358dfc01b8
Yunir Salimzyanov <yunir.me@gmail.com>
2021-04-06 21:29:52 +0300

    Disable stacktrace output in Soap message

--- lab03-server-side/src/main/java/me/yunir/server/App.java
+++ lab03-server-side/src/main/java/me/yunir/server/App.java
@@ -4,6 +4,9 @@ import javax.xml.ws.Endpoint;
 
 public class App {
     public static void main(String[] args) {
+        //disable stacktraces in soap-message
+        System.setProperty("com.sun.xml.ws.fault.SOAPFaultBuilder.disableCaptureStackTrace", "false");
+
         String url = "http://0.0.0.0:8080/PersonService";
         PersonWebService personWebService = new PersonWebService();
 
