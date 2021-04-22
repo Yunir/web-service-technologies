40c09c2627cb249fec98356e7f7defc5be8fe474
Yunir Salimzyanov <yunir.me@gmail.com>
2021-04-22 15:40:40 +0300

    Create base for lab05

---
+++ lab05-client-side/src/main/java/me/yunir/client/WebServiceClient.java
@@ -0,0 +1,42 @@
+package me.yunir.client;
+
+import com.sun.xml.internal.ws.fault.ServerSOAPFaultException;
+import me.yunir.server.Person;
+import me.yunir.server.PersonService;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+
+public class WebServiceClient {
+    public static void main(String[] args) throws MalformedURLException {
+        URL url = new URL("http://localhost:8080/PersonService?wsdl");
+        PersonService personService = new PersonService(url);
+        List<Person> persons = personService.getPersonWebServicePort().getAllPersons();
+        for (Person person : persons) {
+            System.out.println("name: " + person.getName() +
+                    ", surname: " + person.getSurname() +
+                    ", email: " + person.getEmail() +
+                    ", phone: " + person.getPhone() +
+                    ", age: " + person.getAge());
+        }
+        System.out.println("Total persons: " + persons.size());
+        long personId = personService.getPersonWebServicePort().addPerson(
+                "John",
+                "Johnson",
+                "Jhon.Jhonson@gmail.com",
+                "+71112223344",
+                63);
+        System.out.println("Added person id: " + personId);
+        int opCode = personService.getPersonWebServicePort().changePerson(
+                personId,
+                "Mary",
+                "Fedorovna",
+                "Mary.Fedorovna@gmail.com",
+                "+71112223344",
+                63);
+        System.out.println("Updated person id: " + personId + "; Operation code: " + opCode);
+        opCode = personService.getPersonWebServicePort().removePerson(personId);
+        System.out.println("Removed person id: " + personId + "; Operation code: " + opCode);
+    }
+}
---
+++ lab05-server-side/src/main/java/me/yunir/server/PersonWebService.java
@@ -0,0 +1,87 @@
+package me.yunir.server;
+
+import me.yunir.shared.Person;
+import me.yunir.shared.PostgreSQLDAO;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+import java.util.List;
+
+import static me.yunir.server.ConnectionUtil.getConnection;
+
+/**
+ * Веб-сервис.
+ * Содержит операции веб-сервиса.
+ * Содержит инъекцию источника данных, настроенного на стороне сервера приложений.
+ */
+@WebService(serviceName = "PersonService")
+public class PersonWebService {
+
+    @WebMethod(operationName = "addPerson")
+    public long addPerson(
+            @WebParam(name = "personName") String name,
+            @WebParam(name = "personSurname") String surname,
+            @WebParam(name = "personEmail") String email,
+            @WebParam(name = "personPhone") String phone,
+            @WebParam(name = "personAge") int age
+    ) {
+        PostgreSQLDAO dao = new PostgreSQLDAO(getConnection());
+        return dao.addPersons(name, surname, email, phone, age);
+    }
+
+    @WebMethod(operationName = "changePerson")
+    public int changePerson(
+            @WebParam(name = "personId") long id,
+            @WebParam(name = "personName") String name,
+            @WebParam(name = "personSurname") String surname,
+            @WebParam(name = "personEmail") String email,
+            @WebParam(name = "personPhone") String phone,
+            @WebParam(name = "personAge") int age
+    ) {
+        PostgreSQLDAO dao = new PostgreSQLDAO(getConnection());
+        return dao.changePersons(id, name, surname, email, phone, age);
+    }
+
+    @WebMethod(operationName = "removePerson")
+    public int removePerson(@WebParam(name = "personId") long id) {
+        PostgreSQLDAO dao = new PostgreSQLDAO(getConnection());
+        return dao.removePersons(id);
+    }
+
+    @WebMethod(operationName = "getAllPersons")
+    public List<Person> getAllPersons() {
+        PostgreSQLDAO dao = new PostgreSQLDAO(getConnection());
+        return dao.getPersons();
+    }
+
+    @WebMethod(operationName = "getPersonsByName")
+    public List<Person> getPersonsByName(@WebParam(name = "personName") String name) {
+        PostgreSQLDAO dao = new PostgreSQLDAO(getConnection());
+        return dao.getPersonsByName(name);
+    }
+
+    @WebMethod(operationName = "getPersonsBySurname")
+    public List<Person> getPersonsBySurname(@WebParam(name = "personSurname") String surname) {
+        PostgreSQLDAO dao = new PostgreSQLDAO(getConnection());
+        return dao.getPersonsBySurname(surname);
+    }
+
+    @WebMethod(operationName = "getPersonsByEmail")
+    public List<Person> getPersonsByEmail(@WebParam(name = "personEmail") String email) {
+        PostgreSQLDAO dao = new PostgreSQLDAO(getConnection());
+        return dao.getPersonsByEmail(email);
+    }
+
+    @WebMethod(operationName = "getPersonsByPhone")
+    public List<Person> getPersonsByPhone(@WebParam(name = "personPhone") String phone) {
+        PostgreSQLDAO dao = new PostgreSQLDAO(getConnection());
+        return dao.getPersonsByPhone(phone);
+    }
+
+    @WebMethod(operationName = "getPersonsByAge")
+    public List<Person> getPersonsByAge(@WebParam(name = "personAge") String age) {
+        PostgreSQLDAO dao = new PostgreSQLDAO(getConnection());
+        return dao.getPersonsByAge(age);
+    }
+}
