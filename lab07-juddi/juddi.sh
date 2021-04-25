14d3acf6ace568695e367fb1671c1d3c73c8834e
Yunir Salimzyanov <yunir.me@gmail.com>
2021-04-25 14:57:01 +0300

    Implement 7th laboratory work

--- lab07-juddi/juddi.sh
+++ lab07-juddi/juddi.sh
@@ -1,7 +1,33 @@
 #!/usr/bin/env bash
 
-JUDDI_VERSION="3.3.8"
+set -euxo pipefail
 
-wget https://downloads.apache.org/juddi/juddi/$JUDDI_VERSION/juddi-distro-$JUDDI_VERSION.zip
-unzip juddi-distro-$JUDDI_VERSION.zip
-juddi-distro-3.3.8/juddi-tomcat-3.3.8/bin/startup.sh
+# prerequisites
+#add-apt-repository ppa:openjdk-r/ppa
+#apt-get update
+#apt-get install -y unzip openjdk-11-jdk
+
+# JUDDI_VERSION="3.3.3"
+
+# wget https://archive.apache.org/dist/juddi/juddi/$JUDDI_VERSION/juddi-distro-$JUDDI_VERSION.zip
+# unzip juddi-distro-$JUDDI_VERSION.zip
+# juddi-distro-$JUDDI_VERSION/juddi-tomcat-$JUDDI_VERSION/bin/startup.sh
+
+# OR use docker-image
+docker run -v $(pwd):/lab07 --rm -dp 8989:8080 -p 8888:8081 --name web-service-tech-lab07 ericcong/juddi:3.3.3
+docker exec -it web-service-tech-lab07 /bin/bash
+
+# DO THIS on host machine (for lab03)
+# mvn package
+# --
+# apt update
+# apt install -y maven
+# --
+cd /lab07/lab03-server-side
+$JAVA_HOME/bin/java -jar target/lab03-server-side-2.3.jar
+
+# Debian 8 does not have jdk1.8 - it is not necessary if packaged on labstation
+# java -jar target/lab03-server-side-2.0.jar
+# apt-get install software-properties-common
+# add-apt-repository "deb http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main"
+# apt-get update
